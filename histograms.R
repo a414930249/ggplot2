9c16a22f1d72b73827051383c612cefee7474ef0
Dublin R <DublinRUserGroup@gmail.com>
2013-12-17 17:41:50 +0000

    Update histograms.R

--- histograms.R
+++ histograms.R
@@ -1,3 +1,54 @@
+# http://www.cookbook-r.com/Graphs/Plotting_distributions_(ggplot2)/
+
+set.seed(1234)
+df <- data.frame(cond = factor( rep(c("A","B"), each=200) ), 
+                   rating = c(rnorm(200),rnorm(200, mean=.8)))
+# cond     rating
+#    A -1.2070657
+#    A  0.2774292
+#    A  1.0844412
+#   ...
+#    B  1.3388331
+#    B  0.8146431
+#    B -0.1164891
+
+library(ggplot2)
+
+# Basic histogram from the vector "rating". Each bin is .5 wide.
+# These both do the same thing:
+qplot(df$rating, binwidth=.5)
+ggplot(df, aes(x=rating)) + geom_histogram(binwidth=.5)
+
+# Draw with black outline, white fill
+ggplot(df, aes(x=rating)) + geom_histogram(binwidth=.5, colour="black", fill="white")
+
+# Density curve
+ggplot(df, aes(x=rating)) + geom_density()
+
+# Histogram overlaid with kernel density curve
+ggplot(df, aes(x=rating)) + 
+    geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                   binwidth=.5,
+                   colour="black", fill="white") +
+    geom_density(alpha=.2, fill="#FF6666")  # Overlay with transparent density plot
+##Add Line for the mean
+ggplot(df, aes(x=rating)) + geom_histogram(binwidth=.5, colour="black", fill="white") +
+    geom_vline(aes(xintercept=mean(rating, na.rm=T)),   # Ignore NA values for mean
+               color="red", linetype="dashed", size=1)
+################################################################
+
+# Overlaid histograms
+ggplot(df, aes(x=rating, fill=cond)) + geom_histogram(binwidth=.5, alpha=.5, position="identity")
+
+# Interleaved histograms
+ggplot(df, aes(x=rating, fill=cond)) + geom_histogram(binwidth=.5, position="dodge")
+
+# Density plots
+ggplot(df, aes(x=rating, colour=cond)) + geom_density()
+
+# Density plots with semi-transparent fill
+ggplot(df, aes(x=rating, fill=cond)) + geom_density(alpha=.3)
+##################################################################
 # Find the mean of each group
 library(plyr)
 cdf <- ddply(df, "cond", summarise, rating.mean=mean(rating))
